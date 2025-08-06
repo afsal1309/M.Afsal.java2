1ad76bc8803366925f95094d72dd27c0741b2a7f
mdafs <mdafs@Afsal>
2025-08-06 23:44:53 +0530

    initial_commit_2

---
+++ AFSAL_TNS_01/src/TNS01/Voting.java
@@ -0,0 +1,18 @@
+package TNS01;
+
+public class Voting {
+
+	public static void main(String[] args) {
+		int age=11;
+	
+	if(age>=18)
+		System.out.println(" You are eligible for Voting");	
+	else
+		System.out.println("Sorry! You need to wait more");
+
+	System.out.println("if else demonstration");
+		
+
+	}
+
+}
