c28b1affc8b32974f9e597be371bd84a186f9220
mdafs <mdafs@Afsal>
2025-08-10 17:18:49 +0530

    initial commit

---
+++ AFSAL_TNS_01/src/TNS003/Manager.java
@@ -0,0 +1,14 @@
+package TNS003;
+
+import TNS002.Teacher;
+
+public class Manager {
+	public static void main(String[] args) {
+		Teacher s=new Teacher();
+		
+		System.out.println(s.salary);
+		System.out.println(s.name);
+		System.out.println(s.t);}}
+		
+
+
