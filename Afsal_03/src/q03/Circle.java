28f1812605b6fef8289c26c3cec3d0836f935076
mdafs <mdafs@Afsal>
2025-08-21 07:13:01 +0530

    initial commit

---
+++ Afsal_03/src/q03/Circle.java
@@ -0,0 +1,18 @@
+package q03;
+
+public class Circle {
+ private static final String Blue = null;
+{
+		double radius;}
+		void getInput(double r,String c){double radius = r;String colour = c;}
+		void calcArea(){double radius = 0;
+		String colour = Blue;
+		System.out.println("Colour: "+colour+", Area: "+(Math.PI*radius*radius));}
+		public static void main(String[] args){
+		Circle c=new Circle();
+		c.getInput(5.0,"Red"); c.calcArea();
+		 }
+
+
+
+}
