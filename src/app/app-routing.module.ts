2524ef565b1e21c8c7e543c68bcc8786eefc8cad
Mohamed Afsal <mdafsal0915@gmail.com>
2025-09-30 13:40:13 +0530

    Remove nested git repos, add .gitignore, normalize line endings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
+/* app-routing.module.ts */
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
+import { UserFormComponent } from './user-form/user-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/users', pathMatch: 'full' },
+  { path: 'users', component: UserListComponent },
+  { path: 'users/add', component: UserFormComponent },
+  { path: 'users/:id', component: UserDetailComponent },
+  { path: 'users/edit/:id', component: UserFormComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule, {
-  ngZoneEventCoalescing: true,
-})
-  .catch(err => console.error(err));
+import { UserModule } from './app/User.module';
+
+platformBrowserDynamic()
+  .bootstrapModule(UserModule)
+  .catch((err) => console.error(err));
